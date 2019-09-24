e1dda37c95972d96ab6fb48fe65b064c829453f4
Sergey Shostko <siarhei.shostka@instinctools.com>
2019-09-24 15:11:30 +0300

    add error-rx module

---
+++ error-rx/src/main/java/by/shostko/error/rx/Extensions.kt
@@ -0,0 +1,43 @@
+@file:Suppress("unused")
+
+package by.shostko.error.rx
+
+import androidx.annotation.StringRes
+import by.shostko.errors.Error
+import by.shostko.errors.ErrorCode
+import io.reactivex.*
+
+fun <T : Throwable> Observable<T>.asError(): Observable<Error> = map { Error.cast(it) }
+fun <T : Throwable> Flowable<T>.asError(): Flowable<Error> = map { Error.cast(it) }
+fun <T : Throwable> Single<T>.asError(): Single<Error> = map { Error.cast(it) }
+fun <T : Throwable> Maybe<T>.asError(): Maybe<Error> = map { Error.cast(it) }
+
+fun <T> Observable<T>.wrap(code: ErrorCode): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, code)) }
+fun <T> Observable<T>.wrap(id: String): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, id)) }
+fun <T> Observable<T>.wrap(id: String, message: String?): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, id, message)) }
+fun <T> Observable<T>.wrap(id: String, @StringRes resId: Int): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, id, resId)) }
+fun <T> Observable<T>.wrap(id: String, @StringRes resId: Int, vararg args: Any): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, id, resId, args)) }
+
+fun <T> Flowable<T>.wrap(code: ErrorCode): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, code)) }
+fun <T> Flowable<T>.wrap(id: String): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, id)) }
+fun <T> Flowable<T>.wrap(id: String, message: String?): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, id, message)) }
+fun <T> Flowable<T>.wrap(id: String, @StringRes resId: Int): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, id, resId)) }
+fun <T> Flowable<T>.wrap(id: String, @StringRes resId: Int, vararg args: Any): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, id, resId, args)) }
+
+fun <T> Single<T>.wrap(code: ErrorCode): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, code)) }
+fun <T> Single<T>.wrap(id: String): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, id)) }
+fun <T> Single<T>.wrap(id: String, message: String?): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, id, message)) }
+fun <T> Single<T>.wrap(id: String, @StringRes resId: Int): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, id, resId)) }
+fun <T> Single<T>.wrap(id: String, @StringRes resId: Int, vararg args: Any): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, id, resId, args)) }
+
+fun <T> Maybe<T>.wrap(code: ErrorCode): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, code)) }
+fun <T> Maybe<T>.wrap(id: String): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, id)) }
+fun <T> Maybe<T>.wrap(id: String, message: String?): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, id, message)) }
+fun <T> Maybe<T>.wrap(id: String, @StringRes resId: Int): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, id, resId)) }
+fun <T> Maybe<T>.wrap(id: String, @StringRes resId: Int, vararg args: Any): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, id, resId, args)) }
+
+fun Completable.wrap(code: ErrorCode): Completable = onErrorResumeNext { Completable.error(Error.wrap(it, code)) }
+fun Completable.wrap(id: String): Completable = onErrorResumeNext { Completable.error(Error.wrap(it, id)) }
+fun Completable.wrap(id: String, message: String?): Completable = onErrorResumeNext { Completable.error(Error.wrap(it, id, message)) }
+fun Completable.wrap(id: String, @StringRes resId: Int): Completable = onErrorResumeNext { Completable.error(Error.wrap(it, id, resId)) }
+fun Completable.wrap(id: String, @StringRes resId: Int, vararg args: Any): Completable = onErrorResumeNext { Completable.error(Error.wrap(it, id, resId, args)) }
