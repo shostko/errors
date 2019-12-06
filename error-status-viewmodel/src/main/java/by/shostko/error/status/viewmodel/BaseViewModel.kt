2b017d6ca51396fcae2fa92262ae19f5cc757e8c
Sergey Shostko <siarhei.shostka@instinctools.com>
2019-12-06 17:34:50 +0300

    update BaseViewModel to Processing and MultiProcessing super

--- error-status-viewmodel/src/main/java/by/shostko/error/status/viewmodel/BaseViewModel.kt
+++ error-status-viewmodel/src/main/java/by/shostko/error/status/viewmodel/BaseViewModel.kt
@@ -7,8 +7,22 @@ import by.shostko.error.status.ErrorStatusFactory
 import by.shostko.errors.*
 import by.shostko.statushandler.Status
 import by.shostko.statushandler.viewmodel.CustomViewModel
+import by.shostko.statushandler.viewmodel.MultiProcessingViewModel
+import by.shostko.statushandler.viewmodel.ProcessingViewModel
 
-open class BaseViewModel private constructor(mapper: ((Throwable) -> Error)?) : CustomViewModel<Error>(NoError, mapper?.let { ErrorStatusFactory(it) }) {
+open class BaseProcessingViewModel private constructor(mapper: ((Throwable) -> Error)?) : ProcessingViewModel<Error>(NoError, mapper?.let { ErrorStatusFactory(it) }) {
+    constructor() : this(null)
+    constructor(clazz: Class<*>) : this({ it.wrap(SimpleErrorCode(clazz)) })
+    constructor(code: ErrorCode) : this({ it.wrap(code) })
+    constructor(id: String) : this({ it.wrap(id) })
+    constructor(id: String, message: String?) : this({ it.wrap(id, message) })
+    constructor(id: String, @StringRes resId: Int) : this({ it.wrap(id, resId) })
+    constructor(id: String, @StringRes resId: Int, vararg args: Any) : this({ it.wrap(id, resId, args) })
+
+    override fun requireFactory(): Status.Factory<Error> = ErrorStatusFactory { it.wrap(SimpleErrorCode(javaClass)) }
+}
+
+open class BaseMultiProcessingViewModel private constructor(mapper: ((Throwable) -> Error)?) : MultiProcessingViewModel<Error>(NoError, mapper?.let { ErrorStatusFactory(it) }) {
     constructor() : this(null)
     constructor(clazz: Class<*>) : this({ it.wrap(SimpleErrorCode(clazz)) })
     constructor(code: ErrorCode) : this({ it.wrap(code) })
