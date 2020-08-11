72eaa18f63ed2a47f31058d96094698bd70ba879
Sergey Shostko <siarhei.shostka@instinctools.com>
2020-08-11 20:50:58 +0300

    add ErrorCode.builder usage when creating Error

--- error/src/main/java/by/shostko/errors/Extensions.kt
+++ error/src/main/java/by/shostko/errors/Extensions.kt
@@ -9,6 +9,7 @@ fun Throwable.asError(): Error = Error.cast(this)
 fun Throwable.materialize(): Error = Error.materialize(this)
 
 fun Throwable.wrap(code: ErrorCode): Error = Error.wrap(this, code)
+fun Throwable.wrap(builder: ErrorCode.Builder.() -> Unit): Error = Error.wrap(this, builder)
 fun Throwable.wrap(id: String): Error = Error.wrap(this, id)
 fun Throwable.wrap(id: String, message: String?): Error = Error.wrap(this, id, message)
 fun Throwable.wrap(id: String, @StringRes resId: Int): Error = Error.wrap(this, id, resId)
