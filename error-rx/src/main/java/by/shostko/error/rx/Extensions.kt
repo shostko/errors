d15c7c37736881f3bdddf90f1f8c1d4ef35bbd48
Sergey Shostko <siarhei.shostka@instinctools.com>
2020-08-18 18:02:53 +0300

    add withMaterializedError extensions

--- error-rx/src/main/java/by/shostko/error/rx/Extensions.kt
+++ error-rx/src/main/java/by/shostko/error/rx/Extensions.kt
@@ -17,6 +17,7 @@ fun <T : Throwable> Flowable<T>.asMaterializedError(): Flowable<Error> = map { E
 fun <T : Throwable> Single<T>.asMaterializedError(): Single<Error> = map { Error.materialize(it) }
 fun <T : Throwable> Maybe<T>.asMaterializedError(): Maybe<Error> = map { Error.materialize(it) }
 
+fun <T> Observable<T>.withMaterializedError(): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.materialize(th)) }
 fun <T> Observable<T>.wrap(code: ErrorCode): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, code)) }
 fun <T> Observable<T>.wrap(builder: ErrorCode.Builder.() -> Unit): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, builder)) }
 fun <T> Observable<T>.wrap(id: String): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, id)) }
@@ -24,6 +25,7 @@ fun <T> Observable<T>.wrap(id: String, message: String?): Observable<T> = onErro
 fun <T> Observable<T>.wrap(id: String, @StringRes resId: Int): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, id, resId)) }
 fun <T> Observable<T>.wrap(id: String, @StringRes resId: Int, vararg args: Any): Observable<T> = onErrorResumeNext { th: Throwable -> Observable.error(Error.wrap(th, id, resId, args)) }
 
+fun <T> Flowable<T>.withMaterializedError(): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.materialize(th)) }
 fun <T> Flowable<T>.wrap(code: ErrorCode): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, code)) }
 fun <T> Flowable<T>.wrap(builder: ErrorCode.Builder.() -> Unit): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, builder)) }
 fun <T> Flowable<T>.wrap(id: String): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, id)) }
@@ -31,6 +33,7 @@ fun <T> Flowable<T>.wrap(id: String, message: String?): Flowable<T> = onErrorRes
 fun <T> Flowable<T>.wrap(id: String, @StringRes resId: Int): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, id, resId)) }
 fun <T> Flowable<T>.wrap(id: String, @StringRes resId: Int, vararg args: Any): Flowable<T> = onErrorResumeNext { th: Throwable -> Flowable.error(Error.wrap(th, id, resId, args)) }
 
+fun <T> Single<T>.withMaterializedError(): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.materialize(th)) }
 fun <T> Single<T>.wrap(code: ErrorCode): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, code)) }
 fun <T> Single<T>.wrap(builder: ErrorCode.Builder.() -> Unit): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, builder)) }
 fun <T> Single<T>.wrap(id: String): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, id)) }
@@ -38,6 +41,7 @@ fun <T> Single<T>.wrap(id: String, message: String?): Single<T> = onErrorResumeN
 fun <T> Single<T>.wrap(id: String, @StringRes resId: Int): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, id, resId)) }
 fun <T> Single<T>.wrap(id: String, @StringRes resId: Int, vararg args: Any): Single<T> = onErrorResumeNext { th: Throwable -> Single.error(Error.wrap(th, id, resId, args)) }
 
+fun <T> Maybe<T>.withMaterializedError(): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.materialize(th)) }
 fun <T> Maybe<T>.wrap(code: ErrorCode): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, code)) }
 fun <T> Maybe<T>.wrap(builder: ErrorCode.Builder.() -> Unit): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, builder)) }
 fun <T> Maybe<T>.wrap(id: String): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, id)) }
@@ -45,6 +49,7 @@ fun <T> Maybe<T>.wrap(id: String, message: String?): Maybe<T> = onErrorResumeNex
 fun <T> Maybe<T>.wrap(id: String, @StringRes resId: Int): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, id, resId)) }
 fun <T> Maybe<T>.wrap(id: String, @StringRes resId: Int, vararg args: Any): Maybe<T> = onErrorResumeNext { th: Throwable -> Maybe.error(Error.wrap(th, id, resId, args)) }
 
+fun Completable.withMaterializedError(): Completable = onErrorResumeNext { Completable.error(Error.materialize(it)) }
 fun Completable.wrap(code: ErrorCode): Completable = onErrorResumeNext { Completable.error(Error.wrap(it, code)) }
 fun Completable.wrap(builder: ErrorCode.Builder.() -> Unit): Completable = onErrorResumeNext { Completable.error(Error.wrap(it, builder)) }
 fun Completable.wrap(id: String): Completable = onErrorResumeNext { Completable.error(Error.wrap(it, id)) }
