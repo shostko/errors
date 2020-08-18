a2eee02f2ab6fb7be0e9af2d083c5486a0ee9e39
Sergey Shostko <siarhei.shostka@instinctools.com>
2020-08-18 16:03:32 +0300

    adjust BaseViewModel with latest ErrorCode changes

--- error-status-viewmodel/src/main/java/by/shostko/error/status/viewmodel/BaseViewModel.kt
+++ error-status-viewmodel/src/main/java/by/shostko/error/status/viewmodel/BaseViewModel.kt
@@ -4,32 +4,34 @@ package by.shostko.error.status.viewmodel
 
 import androidx.annotation.StringRes
 import by.shostko.error.status.ErrorStatusFactory
-import by.shostko.errors.*
+import by.shostko.errors.Error
+import by.shostko.errors.ErrorCode
+import by.shostko.errors.NoError
+import by.shostko.errors.wrap
 import by.shostko.statushandler.Status
-import by.shostko.statushandler.viewmodel.CustomViewModel
 import by.shostko.statushandler.viewmodel.MultiProcessingViewModel
 import by.shostko.statushandler.viewmodel.ProcessingViewModel
 
 open class BaseProcessingViewModel private constructor(mapper: ((Throwable) -> Error)?) : ProcessingViewModel<Error>(NoError, mapper?.let { ErrorStatusFactory(it) }) {
     constructor() : this(null)
-    constructor(clazz: Class<*>) : this({ it.wrap(SimpleErrorCode(clazz)) })
+    constructor(clazz: Class<*>) : this({ it.wrap { domain(clazz) } })
     constructor(code: ErrorCode) : this({ it.wrap(code) })
     constructor(id: String) : this({ it.wrap(id) })
     constructor(id: String, message: String?) : this({ it.wrap(id, message) })
     constructor(id: String, @StringRes resId: Int) : this({ it.wrap(id, resId) })
     constructor(id: String, @StringRes resId: Int, vararg args: Any) : this({ it.wrap(id, resId, args) })
 
-    override fun requireFactory(): Status.Factory<Error> = ErrorStatusFactory { it.wrap(SimpleErrorCode(javaClass)) }
+    override fun requireFactory(): Status.Factory<Error> = ErrorStatusFactory { it.wrap { domain(javaClass) } }
 }
 
 open class BaseMultiProcessingViewModel private constructor(mapper: ((Throwable) -> Error)?) : MultiProcessingViewModel<Error>(NoError, mapper?.let { ErrorStatusFactory(it) }) {
     constructor() : this(null)
-    constructor(clazz: Class<*>) : this({ it.wrap(SimpleErrorCode(clazz)) })
+    constructor(clazz: Class<*>) : this({ it.wrap { domain(clazz) } })
     constructor(code: ErrorCode) : this({ it.wrap(code) })
     constructor(id: String) : this({ it.wrap(id) })
     constructor(id: String, message: String?) : this({ it.wrap(id, message) })
     constructor(id: String, @StringRes resId: Int) : this({ it.wrap(id, resId) })
     constructor(id: String, @StringRes resId: Int, vararg args: Any) : this({ it.wrap(id, resId, args) })
 
-    override fun requireFactory(): Status.Factory<Error> = ErrorStatusFactory { it.wrap(SimpleErrorCode(javaClass)) }
+    override fun requireFactory(): Status.Factory<Error> = ErrorStatusFactory { it.wrap { domain(javaClass) } }
 }
