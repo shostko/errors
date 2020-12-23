b511958728ed00ceb9c81098c8eb48ddb3aa18ca
Sergey Shostko <s.shostko@gmail.com>
2020-12-23 15:37:04 +0300

    resolve issue with using builder in BaseProcessingViewModel

--- error-status-viewmodel/src/main/java/by/shostko/error/status/viewmodel/BaseViewModel.kt
+++ error-status-viewmodel/src/main/java/by/shostko/error/status/viewmodel/BaseViewModel.kt
@@ -11,7 +11,7 @@ import by.shostko.statushandler.viewmodel.ProcessingViewModel
 
 open class BaseProcessingViewModel private constructor(mapper: ((Throwable) -> Error)?) : ProcessingViewModel<Error>(NoError, mapper?.let { ErrorStatusFactory(it) }) {
     constructor() : this(null)
-    constructor(clazz: Class<*>) : this({ it.wrap { Identifier.Simple(clazz) } })
+    constructor(clazz: Class<*>) : this({ it.wrap(Identifier.Simple(clazz)) })
     constructor(code: ErrorCode) : this({ it.wrap(code) })
     constructor(id: Identifier) : this({ it.wrap(id) })
     constructor(id: Identifier, message: MessageProvider) : this({ it.wrap(id, message) })
@@ -19,12 +19,12 @@ open class BaseProcessingViewModel private constructor(mapper: ((Throwable) -> E
     constructor(id: Identifier, @StringRes resId: Int) : this({ it.wrap(id, resId) })
     constructor(id: Identifier, @StringRes resId: Int, vararg args: Any) : this({ it.wrap(id, resId, args) })
 
-    override fun requireFactory(): Status.Factory<Error> = ErrorStatusFactory { it.wrap { Identifier.Simple(this@BaseProcessingViewModel.javaClass) } }
+    override fun requireFactory(): Status.Factory<Error> = ErrorStatusFactory { it.wrap(Identifier.Simple(this@BaseProcessingViewModel.javaClass)) }
 }
 
 open class BaseMultiProcessingViewModel private constructor(mapper: ((Throwable) -> Error)?) : MultiProcessingViewModel<Error>(NoError, mapper?.let { ErrorStatusFactory(it) }) {
     constructor() : this(null)
-    constructor(clazz: Class<*>) : this({ it.wrap { Identifier.Simple(clazz) } })
+    constructor(clazz: Class<*>) : this({ it.wrap(Identifier.Simple(clazz)) })
     constructor(code: ErrorCode) : this({ it.wrap(code) })
     constructor(id: Identifier) : this({ it.wrap(id) })
     constructor(id: Identifier, message: MessageProvider) : this({ it.wrap(id, message) })
@@ -32,5 +32,5 @@ open class BaseMultiProcessingViewModel private constructor(mapper: ((Throwable)
     constructor(id: Identifier, @StringRes resId: Int) : this({ it.wrap(id, resId) })
     constructor(id: Identifier, @StringRes resId: Int, vararg args: Any) : this({ it.wrap(id, resId, args) })
 
-    override fun requireFactory(): Status.Factory<Error> = ErrorStatusFactory { it.wrap { Identifier.Simple(this@BaseMultiProcessingViewModel.javaClass) } }
+    override fun requireFactory(): Status.Factory<Error> = ErrorStatusFactory { it.wrap(Identifier.Simple(this@BaseMultiProcessingViewModel.javaClass)) }
 }
